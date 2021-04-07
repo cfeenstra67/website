e6f6ed7fad6f633bfe073a32819756b412225886
cfeenstra67 <cameron.l.feenstra@gmail.com>
2021-04-07 00:24:17 -0400

    fixes

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,18 @@
 import Layout from '../components/layout'
+import Config from '../lib/config'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Custom404() {
+export async function getStaticProps() {
+  return {
+    props: {
+      config: Config()
+    }
+  }
+}
+
+export default function Custom404({ config }) {
   return (
-    <Layout>
+    <Layout config={config}>
       <header className={utilStyles.heading2Xl}>Page Not Found</header>
 
       <section className={utilStyles.headingMd}>Try navigating to a different page.</section>
